16964a236ea451fa41fda80156ae2251f588fe1f
musa küçük <musa.kucuk99@gmail.com>
2022-09-14 01:29:20 +0300

    Add project files.

---
+++ Kafka/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace KafkaConsumer // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "messageConsumer",
+                EnableAutoCommit = false,
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+            {
+                consumer.Subscribe("sensor");
+                CancellationTokenSource cancelationToken = new();
+                
+                while(true)
+                {
+                    var consumeResult = consumer.Consume();
+                    Console.WriteLine(consumeResult.Value);
+                    consumer.Commit();
+                }
+
+                consumer.Commit();
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+
+namespace KafkaProducer 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = Dns.GetHostName()
+            };
+
+            Console.WriteLine("************");
+            Console.WriteLine("**Producer**");
+            Console.WriteLine("************");
+
+            using (var producer = new ProducerBuilder<Null,string>(config).Build())
+            {
+                while (true)
+                {
+                    var input = Console.ReadLine();
+                    producer.Produce("sensor", new Message<Null, string>() { Value = input });
+                }
+            }
+        }
+    }
+}
